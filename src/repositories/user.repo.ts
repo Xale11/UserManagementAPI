402be6414e4d5536e7faed117009174218de3435
Alex <alexdiyan.ad@gmail.com>
2025-12-25 21:13:47 +0000

    refactor: updated updated service to talk sql databse

--- src/repositories/user.repo.ts
+++ src/repositories/user.repo.ts
@@ -2,35 +2,36 @@ import type { CreateUserRequestDto } from "../dto/createUserRequest.dto.js"
 import type { UserResponseDto } from "../dto/userResponse.dto.js"
 import { v4 as uuidv4 } from "uuid"
 import { testUsers } from "../constants/testUsers.js"
+import { db } from "../database/pgClient.js"
+import type { QueryResult } from "pg"
 
 export class UserRepository {
 
     private users: UserResponseDto[] = testUsers
 
     public createUser = async (user: CreateUserRequestDto): Promise<UserResponseDto | void> => {
-        const newUser: UserResponseDto = {
-            createdAt: Date.now().toString(),
-            email: user.email,
-            name: user.name,
-            id: uuidv4()
+        const client = await db.connect()
+        try {
+            const data = await client.query('INSERT INTO users (')
+        } finally {
+            client.release()
         }
 
-        this.users.push(newUser)
-
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(newUser)
-            }, 500)
-        })
+        
     }
 
     public getUsers = async (page: number = 1, limit: number = 10): Promise<UserResponseDto[] | void> => {
-        const rangeStart = (page - 1) * limit
-        const rangeEnd = (page * limit)
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve(this.users.slice(rangeStart, rangeEnd))
-            }, 500)
-        })
+        const offset = (page - 1) * limit;
+        const client = await db.connect()
+
+        try {
+            const data = await client.query<UserResponseDto>(
+                'SELECT * FROM users ORDER BY id LIMIT $1 OFFSET $2;',
+                [limit, offset]
+            );
+            return data.rows
+        } finally {
+            client.release()
+        }
     }
 }
